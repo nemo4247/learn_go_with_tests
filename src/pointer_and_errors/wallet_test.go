d51091a39caea79293a1847215e092e59789d1e7
Nemo <nemoling.4247@gmail.com>
2019-03-30 15:05:50 +0700

    支出方法

--- src/pointer_and_errors/wallet_test.go
+++ src/pointer_and_errors/wallet_test.go
@@ -5,14 +5,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(10)
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
