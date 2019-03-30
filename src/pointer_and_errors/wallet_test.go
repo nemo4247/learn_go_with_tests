445e3492f6c4d3b6f9aab441cf02f434a1181889
Nemo <nemoling.4247@gmail.com>
2019-03-30 15:31:38 +0700

    对错误进行重构

--- src/pointer_and_errors/wallet_test.go
+++ src/pointer_and_errors/wallet_test.go
@@ -6,22 +6,6 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, got error, want error) {
-		if got == nil {
-			t.Fatal("wanted an error but didn't get one")
-		}
-		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -29,19 +13,40 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
+	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(10)
+		err := wallet.Withdraw(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
-		startingBalance := Bitcoin(20)
-		wallet := Wallet{startingBalance}
+		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(100))
 
-		assertBalance(t, wallet, startingBalance)
+		assertBalance(t, wallet, Bitcoin(20))
 		assertError(t, err, InsufficientFundsError)
 	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	if got != nil {
+		t.Fatal("got an error but didnt want one")
+	}
+}
+func assertError(t *testing.T, got error, want error) {
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
 }
