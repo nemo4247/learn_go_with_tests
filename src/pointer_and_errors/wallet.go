967531a7dd15c027540a47030b99138cd6c4b004
Nemo <nemoling.4247@gmail.com>
2019-03-30 14:45:35 +0700

    接收者类型改为指针

--- src/pointer_and_errors/wallet.go
+++ src/pointer_and_errors/wallet.go
@@ -1,16 +1,13 @@
 package main
 
-import "fmt"
-
 type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
-	fmt.Println("address of balance in Deposit is", &w.balance)
+func (w *Wallet) Deposit(amount int) {
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
