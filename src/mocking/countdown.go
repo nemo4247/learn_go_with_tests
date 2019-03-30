9699f168463e298a69a38a3ffb39792b1bbd5bfa
Nemo <nemoling.4247@gmail.com>
2019-03-30 17:25:55 +0700

    mocking[未完成]

--- src/mocking/countdown.go
+++ src/mocking/countdown.go
@@ -10,16 +10,52 @@ import (
 const (
 	finalWorld     = "Go!"
 	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
 )
 
-func main() {
-	Countdown(os.Stdout)
+type Sleeper interface {
+	Sleep()
+}
+type SpySleeper struct {
+	Calls int
 }
 
-func Countdown(out io.Writer) {
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+type CountdownOperationSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWorld)
 }
